c96a9145f74a2444aa71f2b3c61a79d34b9095a5
tiffany <tiffany@stormbit.net>
2013-03-16 10:58:20 -0400

    Link highlighting

--- main.lua
+++ main.lua
@@ -80,6 +80,7 @@ local tab = json.decode(body)
 
 function tag(name, args, children)
     local t = {}
+    if not args then args = {} end
     for k, v in pairs(args) do
         t[#t + 1] = string.format("%s=%q", tostring(k), tostring(v))
     end
@@ -95,24 +96,117 @@ function tag(name, args, children)
             children = "\t"..children:gsub("\n", "\t\n")
         end]] --TODO: working tabulator
         local t_s = table.concat(t, " ")
-        return string.format("<%s%s>\n%s\n</%s>", name, t_s == "" and "" or (" "..t_s), children, name)
+        return string.format("<%s%s>%s</%s>", name, t_s == "" and "" or (" "..t_s), children, name)
     end
+end
+
+function convertDate(s)
+    -- from Sat Mar 16 11:45:26 +0000 2013
+    -- to 2013-03-16T11:45:26Z
+    local args
+    if s then
+        local _, _, month_str, day, hour, minute, second, year = s:find("%w+ (%w+) (%d+) (%d+):(%d+):(%d+) %+%d+ (%d+)")
+        local month = ({Jan=1, Feb=2, Mar=3, Apr=4, May=5, Jun=6, Jul=7, Aug=8, Sep=9, Oct=10, Nov=11, Dec=12})[month_str]
+        --print(year, month, day, hour, minute, second)
+        args = {year=year, month=month, day=day, hour=hour, minute=minute, second=second}
+    end
+    return os.date("%Y-%m-%dT%H-%M-%SZ", os.time(args))
+end
+
+function escapeHtml(...)
+    s = table.concat{...}
+    s = s:gsub("&", "&amp;")
+    s = s:gsub("<", "&lt;")
+    s = s:gsub(">", "&gt;")
+    --print(s)
+    return s
+end
+
+function parseEntities(tweet, entities)
+    local ops = {}
+    local media = {}
+    if entities.urls then
+        for k, v in pairs(entities.urls) do
+            ops[#ops+1] = {
+                indices=v.indices,
+                replacement=tag('a', {href=v.expanded_url, title=v.display_url}, v.display_url)
+            }
+        end
+    end
+    if entities.user_mentions then
+        for k, v in pairs(entities.user_mentions) do
+            ops[#ops+1] = {
+                indices=v.indices,
+                replacement=tag('a', {href="https://twitter.com/"..v.screen_name, title=v.name}, "@"..v.screen_name)
+            }
+        end
+    end
+    if entities.hashtags then
+        for k,v in pairs(entities.hashtags) do
+            ops[#ops+1] = {
+                indices=v.indices,
+                replacement=tag('a', {href="https://twitter.com/search?q="..v.text}, "#"..v.text)
+            }
+        end
+    end
+    if entities.media then
+        for k,v in pairs(entities.media) do
+            ops[#ops+1] = {
+                indices=v.indices,
+                replacement=tag('a', {href=v.exanded_url}, v.display_url)
+            }
+            media[#media+1] = v.media_url
+        end
+    end
+    table.sort(ops, function(a,b) return a.indices[1] < b.indices[1] end)
+    local offset = 0
+    for i=1, #ops do
+        local v = ops[i]
+        tweet = tweet:sub(1, v.indices[1] + offset) .. v.replacement .. tweet:sub(v.indices[2] + offset + 1, -1)
+        offset = offset + (#v.replacement - (v.indices[2]-v.indices[1]))
+    end
+    return tweet, media
 end
 
 local children = {}
 
 children[1] = tag('title', {}, "Twitter Home Timeline")
 children[2] = tag('link', {href="https://twitter.com"})
+children[3] = tag('updated', {}, convertDate())
 
 for k, v in pairs(tab) do
+    local tweet, media = parseEntities(v.text, v.entities)
     children[#children+1] = tag('entry', {}, {
-        tag('summary', {},
+        tag('title', {},
             v.text
         ),
+        tag('content', {type="html"}, {escapeHtml(
+            tag('div', {}, {
+                tag('a', {href="https://twitter.com/"..v.user.screen_name}, {
+                    tag('img', {style='float:left', src=v.user.profile_image_url}),
+                    v.user.name, 
+                    tag('br'),
+                    "@"..v.user.screen_name,
+                }),
+                v.user.protected and " - private" or "",
+            }),
+            tag('hr'),
+            tweet,
+            tag('br'),
+            v.source
+        )}),
         tag('author', {},
             tag('name', {},
-                v.user.name
-            )
+                v.user.name .. " @" .. v.user.screen_name
+            ),
+            tag('link', {href="https://twitter.com/"..v.user.screen_name})
+        ),
+        tag('link', {href="https://twitter.com/"..v.user.screen_name.."/statuses/"..v.id_str}),
+        tag('published', {},
+            convertDate(v.created_at)
+        ),
+        tag('id', {},
+            v.id_str
         )
     })
 end
